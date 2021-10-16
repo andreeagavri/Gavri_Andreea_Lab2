0d82ad3d5b097dd86ba490916f4604c40fb8a2a5
andreeagavri <andreea.gavri@yahoo.ro>
2021-10-16 16:24:22 +0300

    Add project files.

---
+++ Gavri_Andreea_Lab2/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Gavri_Andreea_Lab2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
